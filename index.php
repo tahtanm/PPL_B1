da72b5963abd58243f5e0ba255edc38280297460
tahtanm <89627836+tahtanm@users.noreply.github.com>
2022-04-20 22:48:04 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        body{
+            
+            background-image: url("Assets/images/various-chips-bowls-marble-surface.jpg");
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+            background-attachment: fixed;
+            height: 100%;
+           
+      }
+    </style>
+
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage Pelanggan</title>
+    <link rel="stylesheet" href="Assets/css/bootstrap.css">
+</head>
+<body>
+    <div class="container-fluid">
+
+    </div>
+    <div class="container my-5">
+        <div class="title text-center">
+        <h1 class="judul-index">Selamat Datang Di Kres.co!</h1>
+                <a class="btn btn-secondary btn-lg" href="registrasi.php">Daftar</a> 
+                <a class="btn btn-secondary btn-lg" href="login.php">Masuk</a>
+            </p>
+        </div>
+    </div>
+</body>
+</html>
